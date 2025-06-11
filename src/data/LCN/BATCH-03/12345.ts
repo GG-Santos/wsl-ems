890c2ee6f8d4ac097fee243580d6cbefdad0f863
Gravitium <gino041504arwrin@gmail.com>
2025-06-11 23:46:11 +0800

    Initial commit - overwriting remote

---
+++ src/data/LCN/BATCH-03/12345.ts
@@ -0,0 +1,15 @@
+const lcnData = {
+  lcn: "12345",
+  name: "Gino Arwin G. Santos",
+  issued: "August 04, 2024",
+  expiration: "August 04, 2025",
+  registration: "August 04, 2024",
+  FWE: "09",
+  SJE: "22",
+  EP: "08",
+  PAS: "10",
+  CCST: "20",
+  CCSM: "17",
+}
+
+export default lcnData
---
+++ src/data/LCN/BATCH-03/LCN_Data.ts
@@ -0,0 +1,6 @@
+// data/LCN/BATCH-02/LCN_Data.ts
+const validLCNs = [
+  "BEMT082024014",
+];
+
+export default validLCNs;
