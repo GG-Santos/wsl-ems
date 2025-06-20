3b3103bc9747aa606dd850a4595fa64df6995d62
GG-Santos <ggsantos_0415@proton.me>
2025-06-21 01:50:32 +0800

    JAY

---
+++ src/data/LCN/BATCH-09/BEMT082024015.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "BEMT082024015",
+  name: "JAY P. CAL",
+  issued: "August 04, 2024",
+  expiration: "August 04, 2026",
+  registration: "August 04, 2024",
+  FWE: "8.9",
+  SJE: "8.9",
+  EP: "7.5",
+  PAS: "16.2",
+  CCST: "18",
+  CCSM: "24.5",
+  batch: "BATCH-09",
+  ranking: "PASSED"
+}
+
+export default lcnData
--- src/data/LCN/BATCH-09/LCN_Data.ts
+++ src/data/LCN/BATCH-09/LCN_Data.ts
@@ -1,7 +1,8 @@
 // data/LCN/BATCH-02/LCN_Data.ts
 const validLCNs = [
   "TEMPLATE-09",
-  "BEMT082024014"
+  "BEMT082024014",
+  "BEMT082024015",
 ];
 
 export default validLCNs;
