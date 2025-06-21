51ea1ea805e12fa7f2dd54b45aaa3be4392247df
GG-Santos <ggsantos_0415@proton.me>
2025-06-21 11:06:45 +0800

    15

---
+++ src/data/LCN/BATCH-15/TEMPLATE-15.ts
@@ -0,0 +1,17 @@
+const lcnData = {
+  lcn: "TEMPLATE-15",
+  name: "Legal Name",
+  issued: "Issued Date",
+  expiration: "Expiry Date",
+  registration: "Latest Registration Date",
+  FWE: "00",
+  SJE: "00",
+  EP: "00",
+  PAS: "00",
+  CCST: "00",
+  CCSM: "00",
+  batch: "BATCH-15",
+  ranking: "0"
+}
+
+export default lcnData
