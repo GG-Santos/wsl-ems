5f7946e3290a86b4438f7470fd2116df677453e7
FEPAG5 <fepag_05@yahoo.com>
2025-06-21 10:58:52 +0800

    Update A12-2505601.ts

--- src/data/LCN/BATCH-16/A12-2505601.ts
+++ src/data/LCN/BATCH-16/A12-2505601.ts
@@ -1,1 +1,17 @@
+const lcnData = {
+  lcn: "A12-2505601",
+  name: "Quinn",
+  issued: "July 05, 2025",
+  expiration: "July 05, 2026",
+  registration: "July 05, 2025",
+  FWE: "10",
+  SJE: "20",
+  EP: "10",
+  PAS: "10",
+  CCST: "25",
+  CCSM: "25",
+  batch: "BATCH-16",
+  ranking: "1"
+}
 
+export default lcnData
